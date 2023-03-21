0c08c08ae0b794bdcf90f5df7e69e4e444d7580e
bmnimashi@gmail.com <bmnimashi@gmail.com>
2023-03-21 18:34:03 +0530

    SignUp-added

--- src/com/developersstack/medec/controller/SignUpFormController.java
+++ src/com/developersstack/medec/controller/SignUpFormController.java
@@ -2,21 +2,19 @@ package com.developersstack.medec.controller;
 
 import com.developersstack.medec.db.Database;
 import com.developersstack.medec.dto.UserDto;
+import com.developersstack.medec.enums.AccountType;
 import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Optional;
-
-import static com.sun.scenario.effect.impl.sw.sse.SSEPhongLighting_DISTANTPeer.filter;
-import static jdk.nashorn.internal.objects.NativeArray.forEach;
 
 public class SignUpFormController {
     public AnchorPane signUpContext;
@@ -24,15 +22,14 @@ public class SignUpFormController {
     public JFXTextField txtLastName;
     public JFXTextField txtEmail;
     public JFXPasswordField txtPassword;
+    public JFXRadioButton rdtndoctor;
 
 
     public void AlreadyHaveAnAccountOnAction(ActionEvent actionEvent) throws IOException {
-        Stage stage = (Stage)signUpContext.getScene().getWindow();
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
-
+        rediretUI();
     }
 
-    public void signUpOnAction(ActionEvent actionEvent) {
+    public void signUpOnAction(ActionEvent actionEvent) throws IOException {
         // users with same email should be restricted
         String email = txtEmail.getText();
 //        for(UserDto dto:Database.userTable){
@@ -45,8 +42,24 @@ public class SignUpFormController {
                 .filter(e -> e.getEmail().equals(email.trim().toLowerCase()))
                 .findFirst();
         if(selectedUser.isPresent()){
-            new Alert(Alert.AlertType.WARNING,"Email is already exists!");
+            new Alert(Alert.AlertType.WARNING,"Email is already exists!").show();
+            return ;
         }
+
+        Database.userTable.add(
+            new UserDto(txtFirstName.getText(),
+                    txtLastName.getText(),
+                    email,
+                    txtPassword.getText(),
+                    rdtndoctor.isSelected()? AccountType.DOCTOR :AccountType.PATIENT)
+        );
+        new Alert(Alert.AlertType.CONFIRMATION,"Welcome!").show();
+        rediretUI();
+    }
+
+    private void rediretUI() throws IOException {
+        Stage stage = (Stage)signUpContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
 
     }
 }

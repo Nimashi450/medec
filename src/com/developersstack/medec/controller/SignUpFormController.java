2dd0ccff8ae717dcd87c840db74494d08db0205a
bmnimashi@gmail.com <bmnimashi@gmail.com>
2023-03-21 18:07:24 +0530

    SignUp-added

--- src/com/developersstack/medec/controller/SignUpFormController.java
+++ src/com/developersstack/medec/controller/SignUpFormController.java
@@ -1,15 +1,22 @@
 package com.developersstack.medec.controller;
 
+import com.developersstack.medec.db.Database;
+import com.developersstack.medec.dto.UserDto;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
+
+import static com.sun.scenario.effect.impl.sw.sse.SSEPhongLighting_DISTANTPeer.filter;
+import static jdk.nashorn.internal.objects.NativeArray.forEach;
 
 public class SignUpFormController {
     public AnchorPane signUpContext;
@@ -26,5 +33,20 @@ public class SignUpFormController {
     }
 
     public void signUpOnAction(ActionEvent actionEvent) {
+        // users with same email should be restricted
+        String email = txtEmail.getText();
+//        for(UserDto dto:Database.userTable){
+//            if(dto.getEmail().equals(email.trim().toLowerCase())){
+//                new Alert(Alert.AlertType.WARNING, "Email is already existed");
+//                return ;
+//            }
+//        }
+        Optional<UserDto> selectedUser = Database.userTable.stream()
+                .filter(e -> e.getEmail().equals(email.trim().toLowerCase()))
+                .findFirst();
+        if(selectedUser.isPresent()){
+            new Alert(Alert.AlertType.WARNING,"Email is already exists!");
+        }
+
     }
 }

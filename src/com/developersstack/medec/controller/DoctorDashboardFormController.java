0d79fe155fdd753e5561e9588ed907badcf1dccd
bmnimashi@gmail.com <bmnimashi@gmail.com>
2023-03-22 00:35:31 +0530

    doctor-dashboard

--- src/com/developersstack/medec/controller/DoctorDashboardFormController.java
+++ src/com/developersstack/medec/controller/DoctorDashboardFormController.java
@@ -2,14 +2,20 @@ package com.developersstack.medec.controller;
 
 import com.developersstack.medec.utill.Cookie;
 import com.sun.org.apache.bcel.internal.generic.DADD;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DoctorDashboardFormController {
@@ -28,6 +34,18 @@ public class DoctorDashboardFormController {
 //        String today = simpleDateFormat.format(date);
 //        lbldate.setText(today);
         lbldate.setText(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0),
+                e ->{
+                    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm:ss");
+                    String time = LocalTime.now().format(dtf);
+                    lbltime.setText(time);
+                }
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     public void checkUser() throws IOException {
---
+++ src/com/developersstack/medec/dto/DoctorDto.java
@@ -0,0 +1,5 @@
+package com.developersstack.medec.dto;
+
+public class DoctorDto {
+
+}
